eb01c95df57667b8df7544b9d2b5c51442fc07f6
bitinbyte <bitinbyte@outlook.com>
2020-09-24 19:22:04 +0100

    Personal website added to footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,16 +1,35 @@
 //Import libraries
-import React from 'react';
+import React from "react";
 
 //Import components
 
 //Import scoped class modules
-import classes from './Footer.module.scss';
+import classes from "./Footer.module.scss";
 
 //Stateless component
 const footer = () => (
-    <div className={classes.Footer}>
-        Made with <span role="img" aria-label="love"> ❤️ </span> for all devs around the <span role="img" aria-label="world"> 🌍 </span>!! Made by &copy;BitInByte
-    </div>
+  <div className={classes.Footer}>
+    <p>
+      Made with
+      <span role="img" aria-label="love">
+        {" "}
+        ❤️{" "}
+      </span>
+      for all devs around the
+      <span role="img" aria-label="world">
+        {" "}
+        🌍
+      </span>
+      !! Made by{" "}
+      <a
+        href="https://www.jopecodes.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        &copy;BitInByte
+      </a>
+    </p>
+  </div>
 );
 
 export default footer;
